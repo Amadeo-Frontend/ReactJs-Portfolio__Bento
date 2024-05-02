0e68a090b7777cbe856ee4bcc9958249a70139fa
Amadeo Bon <amadeosph@outlook.com>
2024-05-02 17:16:02 -0300

    Remove text

--- src/components/AboutBlock.tsx
+++ src/components/AboutBlock.tsx
@@ -9,10 +9,7 @@ const AboutBlock = () => {
         como ReactJS, Angular e React Native. {""}
         <span className="text-zinc-400">
           Sou capaz de construir interfaces responsivas e dinâmicas para web e
-          dispositivos móveis. Minha abordagem centrada no usuário e minha
-          habilidade em traduzir requisitos de design em código eficiente
-          garantem que cada projeto seja entregue com alta qualidade e
-          usabilidade. Estou sempre buscando aprender e me atualizar nas últimas
+          dispositivos móveis. Estou sempre buscando aprender e me atualizar nas últimas
           tendências e melhores práticas do desenvolvimento frontend para
           proporcionar soluções de ponta aos meus clientes e usuários.
         </span>
